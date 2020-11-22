f6813915f6b129a82c64c076c3c54eb9e5115c3d
Walker <kimaniwalker@gmail.com>
2020-11-22 02:48:05 -0600

    Added Styling

--- client/src/components/screens/Recipe/create-cookbook.jsx
+++ client/src/components/screens/Recipe/create-cookbook.jsx
@@ -7,6 +7,8 @@ export default function CreateCookbook(props) {
 
 
 
+
+
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log('clicked')
@@ -44,103 +46,164 @@ export default function CreateCookbook(props) {
         <>
             <Header />
             <main className="cookbook">
-                <div className="container bg-light py-4">
-                    <div className="row py-4 justify-content-center">
+                <div id="cookbookcontainer" className="container mb-5">
+                    <div className="row py-4 justify-content-center py-4">
                         <h2 className="text-dark outline">Create A Recipe</h2>
                     </div>
-                </div>
+
+                    <div className="row justify-content-center d-flex flex-wrap my-2 py-5">
+
+                        <div className="stepBorder">
+                            <h6 className="pt-2">{props.values.step}</h6>
+                        </div>
+
+                        <div className="m-2">
+                            <span>Create Cookbook</span>
+                            <hr />
+                        </div>
+
+                        <div className="stepBorder remove">
+                            <h6 className="pt-2">{props.values.step + 1}</h6>
+                        </div>
+                        <div className="m-2 remove">
+                            <span>Add Ingredients</span>
+                            <hr />
+                        </div>
+                        <div className="stepBorder remove">
+                            <h6 className="pt-2">{props.values.step + 2}</h6>
+                        </div>
+                        <div className="m-2 remove">
+                            <span>Add Directions</span>
+                            <hr />
+                        </div>
+                        <div className="stepBorder remove">
+                            <h6 className="pt-2">{props.values.step + 3}</h6>
+                        </div>
+                        <div className="m-2 remove">
+                            <span>Add Tags</span>
+                            <hr />
+                        </div>
 
 
 
-                <div className="container bg-light">
-                    <div className="row justify-content-center">
-                        <form onSubmit={handleSubmit}>
 
-                            <div className="row justify-content-center">
-                                <div id="name">
-
-                                    <input id="name" type="text"
-                                        placeholder={'Recipe Name'}
-                                        onChange={(e) => props.handleNameChange(e.target.value)}
-                                        values={props.values.name}
-                                        className="input-group-lg"
-                                    />
-                                </div>
-                            </div>
-
-
-
-                            <div className="row justify-content-center">
-                                <div>
-
-                                    <input id="servingSize" type="text"
-                                        placeholder={'Serving Size'}
-                                        onChange={(e) => props.handleServingSizeChange(e.target.value)}
-                                        values={props.values.servingSize} />
-                                </div>
-                            </div>
-                            <div className="row justify-content-center">
-                                <div>
-
-                                    <input id="calories" type="text"
-                                        placeholder={'Calories'}
-                                        onChange={(e) => props.handleCaloriesChange(e.target.value)}
-                                        values={props.values.calories} />
-                                </div>
-                            </div>
-                            <div className="row justify-content-center">
-                                <div>
-
-                                    <input id="skillLevel" type="text"
-                                        placeholder={'Skill Level'}
-                                        onChange={(e) => props.handleSkillChange(e.target.value)}
-                                        values={props.values.skillLevel} />
-                                </div>
-
-                            </div>
-
-                            <div className="row justify-content-center">
-                                <div>
-                                    <div id="upload-box">
-                                        <input type="file" onChange={(e) => props.handleImageChange(e.target.files[0])} />
-
-                                    </div>
-                                </div>
-                            </div>
-
-                        </form>
                     </div>
 
 
 
 
-
-                    {props.values.name.length < 1 || props.values.tags.length < 1 || props.values.servingSize.length < 1 ?
-
-                        <div className="container">
-                            <div className="row justify-content-center">
-
-                                <span className="warning py-4 text-danger">* Please Fill Out All Form Items To Proceed</span>
-
-                            </div>
-
-                        </div>
-                        : null
-                    }
                     <div className="container">
                         <div className="row justify-content-center">
+                            <form onSubmit={handleSubmit}>
+
+                                <div className="row justify-content-center">
+                                    <div id="name">
+
+                                        <input id="name" type="text"
+                                            placeholder={'Recipe Name'}
+                                            onChange={(e) => props.handleNameChange(e.target.value)}
+                                            value={props.values.name}
+                                            className="input-group-lg"
+                                        />
+                                    </div>
+                                </div>
 
 
 
-                            <span className="continue text-success py-5"
-                                onClick={next}
-                                disabled={props.values.name.length < 1 || props.values.tags.length < 1 || props.values.servingSize.length < 1}><i class="fas fa-arrow-circle-right fa-3x"></i></span>
+                                <div className="row justify-content-center">
+                                    <div>
 
+                                        <input id="servingSize" type="text"
+                                            placeholder={'Serving Size'}
+                                            onChange={(e) => props.handleServingSizeChange(e.target.value)}
+                                            value={props.values.servingSize} />
+                                    </div>
+                                </div>
+                                <div className="row justify-content-center">
+                                    <div>
 
+                                        <input id="calories" type="text"
+                                            placeholder={'Calories'}
+                                            onChange={(e) => props.handleCaloriesChange(e.target.value)}
+                                            value={props.values.calories} />
+                                    </div>
+                                </div>
+                                <div className="row justify-content-center">
+                                    <div>
 
+                                        <input id="skillLevel" type="text"
+                                            placeholder={'Skill Level'}
+                                            onChange={(e) => props.handleSkillChange(e.target.value)}
+                                            value={props.values.skillLevel} />
+                                    </div>
+
+                                </div>
+
+                                <div className="row justify-content-center">
+                                    <div>
+                                        <div id="upload-box">
+                                            <input type="file" onChange={(e) => props.handleImageChange(e.target.files[0])
+                                                } />
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </form>
                         </div>
 
+
+                        <div className="container">
+                            <div className="row justify-content-center py-4">
+                            {props.values.name.length < 1 || props.values.tags.length < 1 || props.values.servingSize.length < 1 ?
+
+<div className="container">
+    <div className="row justify-content-center">
+
+        <span className="warning py-4 text-danger">* Please Fill Out All Form Items To Proceed</span>
+
+    </div>
+
+</div>
+: null
+}
+                            </div>
+                        </div>
+
+
+                        
+                        <div className="container py-4">
+                            <div className="row justify-content-center">
+
+
+                                <div>
+                                    <span className="continue text-success py-5">
+
+
+
+                                        <button className="btn" onClick={next}
+                                        disabled={props.values.skillLevel.length < 1
+                                        || props.values.calories.length < 1 || props.values.name.length < 1 ||
+                                    props.values.servingSize.length < 1 || props.values.images.length < 1}
+                                        
+
+                                        ><i className="fas fa-arrow-circle-right fa-2x"></i>
+                                        </button>
+
+                                    </span>
+                                </div>
+
+
+
+
+
+
+
+                            </div>
+
+                        </div>
                     </div>
+
                 </div>
 
 
--- client/src/components/screens/Recipe/create-ingredients.jsx
+++ client/src/components/screens/Recipe/create-ingredients.jsx
@@ -19,116 +19,134 @@ export default function CreateIngredients(props) {
         props.handleAddIngredient(e.target.value);
     }
 
+    
 
     return (
         <>
             <Header />
             <main className="ingredients">
 
-
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <h2 className="mb-5 outline">Add Ingredients</h2>
-                    </div>
+                <div id="ingredientsContainer" className="container py-5 my-4">
 
 
-                </div>
-
-                <div className="container py-4">
-                    <div className="row justify-content-center d-flex flex-wrap">
-
-                        <div className="stepBorder remove">
-                            <h6 className="pt-2">{props.values.step - 1}</h6>
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <h2 className="mb-5 outline">Add Ingredients</h2>
                         </div>
-
-                        <div className="m-2 remove">
-                            <span>Create Cookbook</span>
-                            <hr />
-                        </div>
-
-                        <div className="stepBorder">
-                            <h6 className="pt-2">{props.values.step + 0}</h6>
-                        </div>
-                        <div className="m-2">
-                            <span>Add Ingredients</span>
-                            <hr />
-                        </div>
-                        <div className="stepBorder">
-                            <h6 className="pt-2">{props.values.step + 1}</h6>
-                        </div>
-                        <div className="m-2">
-                            <span>Add Directions</span>
-                            <hr />
-                        </div>
-                        <div className="stepBorder remove">
-                            <h6 className="pt-2">{props.values.step + 2}</h6>
-                        </div>
-                        <div className="m-2 remove">
-                            <span>Submit</span>
-                            <hr />
-                        </div>
-
-
 
 
                     </div>
 
-                    <div className="row justify-content-center pt-5">
-                        <div className="form-group">
-                            
-                            <input type="text" className="input-group" name="ingredient" onChange={handleIngredient}
-                                value={props.values.ingredient}
-                                placeholder="Add Ingredient" />
+                    <div className="row justify-content-center d-flex flex-wrap py-5 my-2">
+
+                            <div className="stepBorder">
+                                <h6 className="pt-2">{props.values.step}</h6>
+                            </div>
+
+                            <div className="m-2">
+                                <span>Add Ingredients</span>
+                                <hr />
+                            </div>
+
+                            <div className="stepBorder remove">
+                                <h6 className="pt-2">{props.values.step + 1}</h6>
+                            </div>
+                            <div className="m-2 remove">
+                                <span>Add Directions</span>
+                                <hr />
+                            </div>
+                            <div className="stepBorder remove">
+                                <h6 className="pt-2">{props.values.step + 2}</h6>
+                            </div>
+                            <div className="m-2 remove">
+                                <span>Add Tags</span>
+                                <hr />
+                            </div>
+                            <div className="stepBorder remove">
+                                <h6 className="pt-2">{props.values.step + 3}</h6>
+                            </div>
+                            <div className="m-2 remove">
+                                <span>Submit</span>
+                                <hr />
+                            </div>
+
+
+
+
                         </div>
-                    </div>
 
-
-                </div>
-
-
-
-
-                <div className="container mb-5">
-                    <div className="row justify-content-center">
-                        <span className="continue text-light py-5 mx-3"
-                            onClick={back}
-                        ><i className="fas fa-arrow-circle-left fa-3x"></i>
-                        </span>
-                        <span className="continue text-light py-5 mx-3"
-                            disabled={props.values.ingredientList.length < 1}
-                            onClick={(e) => props.handleIngredientPush()}
-                        ><i class="fas fa-plus-circle fa-3x"></i>
-                        </span>
-                        <span className="continue text-light py-5 mx-3"
-                            onClick={next}
-                            disabled={props.values.ingredientList.length < 1}><i className="fas fa-arrow-circle-right fa-3x"></i>
-                        </span>
-                    </div>
-                </div>
-
-                {props.values.ingredientList.length >= 1 ? <div className="container py-5">
-                    <div className="row justify-content-center">
-                        <h4>Ingredient List</h4>
-                    </div>
-                    <div className="row justify-content-center">
-                      <ol>
-                        {props.values.ingredientList.map((ingredientitem) => {
-                            return <li className="py-3" key={ingredientitem}>{ingredientitem}</li>
-                        })}
-                    </ol>  
-                    </div>
-                    
-
-                </div>
-                    : <div className="container py-5">
-                    <div className="row justify-content-center">
-                        <h4>Ingredient List:</h4>
+                    <div className="container py-4">
                         
+
+                        <div className="row justify-content-center pt-5">
+                            <div className="form-group">
+
+                                <input type="text" className="input-group" name="ingredient" onChange={(e) => props.handleAddIngredient(e.target.value)}
+                                    value={props.values.ingredient}
+                                    placeholder="Add Ingredient" />
+                            </div>
+                        </div>
+
+
                     </div>
-                    <div className="row justify-content-center">
-                        <div className="text-warning"><span>Your List Is Currently Empty</span></div>
+
+
+
+
+                    <div className="container mb-5">
+                        <div className="row justify-content-center">
+                            <span 
+                            >
+                                
+                                <button className=" btn continue py-5 mx-3"
+                                onClick={back}><i className="fas fa-arrow-circle-left fa-2x"></i></button>
+                                
+                                
+                                
+                            </span>
+                            <span 
+                            >
+                                
+                                <button className="btn continue py-5 mx-3"
+                                disabled={props.values.ingredient.length < 1}
+                                onClick={(e) => props.handleIngredientPush()}><i class="fas fa-plus-circle fa-2x"></i></button>
+                                
+                                
+                            </span>
+                            <span >
+                                    <button className=" btn continue py-5 mx-3"
+                                onClick={next}
+                                disabled={props.values.ingredientList.length < 1}><i className="fas fa-arrow-circle-right fa-2x"></i></button>
+                                    
+                                    
+                            </span>
+                        </div>
                     </div>
-                    </div>}
+
+                    {props.values.ingredientList.length >= 1 ? <div className="container py-5">
+                        <div className="row justify-content-center">
+                            <h4>Ingredient List</h4>
+                        </div>
+                        <div className="row justify-content-center">
+                            <ol>
+                                {props.values.ingredientList.map((ingredientitem) => {
+                                    return <li className="py-3" key={ingredientitem}>{ingredientitem}</li>
+                                })}
+                            </ol>
+                        </div>
+
+
+                    </div>
+                        : <div className="container py-5">
+                            <div className="row justify-content-center">
+                                <h4>Ingredient List:</h4>
+
+                            </div>
+                            <div className="row justify-content-center">
+                                <div className="text-warning"><span>Your List Is Currently Empty</span></div>
+                            </div>
+                        </div>}
+                </div>
 
 
 
